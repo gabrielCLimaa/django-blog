7e1b74f49c96bd2df68c163e7f81d0a4f81c5f6d
gabrielCLimaa <gabrielcrcl12345@gmail.com>
2021-12-15 11:03:55 -0300

    Fix

--- myblog/settings.py
+++ myblog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from .local_settings import *
 from pathlib import Path
 import os
 
@@ -19,18 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-02$j$#^)$&^0+d8fm_q5c-8ked9zabz_q2xvs9jo&e+q$%p3au'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    '.pythonanywhere.com'
-]
-
 
 # Application definition
 
